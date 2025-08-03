32e379a29e47eb50adc8c8d8b037ae9bd075120e
Daniel Kee <48283485+dkly12345@users.noreply.github.com>
2025-08-03 14:37:35 +0800

    BookService interface init

---
+++ src/test/java/com/example/demo/service/BookService.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.BookDto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookService {
+    List<BookDto> findAll();
+    Optional<BookDto> findById(Long id);
+    Optional<BookDto> save(BookDto bookDto);
+    Optional<BookDto> update(Long id, BookDto bookDto);
+    boolean delete(Long id);
+}
